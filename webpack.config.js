16be9a2235037b63e898ac1c477198d8d9fae9bb
Ram Alsaifi <r.r.alsaifi@gmail.com>
2023-03-18 17:02:42 +1100

    Added html-webpack-plugin module

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -6,5 +7,6 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"), 
         filename: "bundle.js"
-    }
+    }, 
+    plugins: [new HtmlWebpackPlugin({template: "./src/index.html"})]
 }
